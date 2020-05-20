250756cef343c123f5a2eb2f1021e936ed7bccca
Phil Adams <phil_adams@us.ibm.com>
2020-05-19 23:33:34 -0500

    feat(Case Management): add service to project (#22)

--- ibm_platform_services/__init__.py
+++ ibm_platform_services/__init__.py
@@ -22,6 +22,7 @@ from ibm_cloud_sdk_core import IAMTokenManager, DetailedResponse, BaseService, A
 from .common import get_sdk_headers
 from .version import __version__
 
+from .case_management_v1 import CaseManagementV1
 from .global_catalog_v1 import GlobalCatalogV1
 from .global_search_v2 import GlobalSearchV2
 from .global_tagging_v1 import GlobalTaggingV1
