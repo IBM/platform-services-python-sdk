2c46c4452e1922d2a030e103d125c93122dff70a
Phil Adams <phil_adams@us.ibm.com>
2020-09-29 19:39:53 -0500

    test: fix examples

--- examples/test_iam_identity_v1_examples.py
+++ examples/test_iam_identity_v1_examples.py
@@ -17,6 +17,7 @@
 Examples for IamIdentityV1
 """
 
+import json
 import os
 import pytest
 from ibm_cloud_sdk_core import ApiException, read_external_sources
@@ -29,14 +30,17 @@ iam_identity_service = None
 
 config = None
 
-apikeyName = 'Python-SDK-IT-ApiKey'
-serviceIDName = 'Python-SDK-IT-ServiceId'
-newDescription = 'This is an updated description'
+apikey_name = 'Example-ApiKey'
+serviceid_name = 'Example-ServiceId'
 
-accountId = None
-iamId = None
+account_id = None
+iam_id = None
 apikey = None
 
+apikey_id = None
+apikey_etag = None
+svc_id = None
+svc_id_etag = None
 
 
 ##############################################################################
@@ -57,7 +61,7 @@ class TestIamIdentityV1Examples():
             # begin-common
 
             iam_identity_service = IamIdentityV1.new_instance()
-            
+
             # end-common
             assert iam_identity_service is not None
 
@@ -65,10 +69,15 @@ class TestIamIdentityV1Examples():
             global config
             config = read_external_sources(IamIdentityV1.DEFAULT_SERVICE_NAME)
 
-            accountId = config['ACCOUNT_ID']
-            iamId = config['IAM_ID']
+            global account_id
+            account_id = config['ACCOUNT_ID']
+
+            global iam_id
+            iam_id = config['IAM_ID']
+
+            global apikey
             apikey = config['APIKEY']
-        
+
         print('Setup complete.')
 
     needscredentials = pytest.mark.skipif(
@@ -81,60 +90,20 @@ class TestIamIdentityV1Examples():
         create_api_key request example
         """
         try:
+            global apikey_name, iam_id, apikey_id
+
             # begin-create_api_key
 
             api_key = iam_identity_service.create_api_key(
-                name= apikeyName,
-                iam_id= iamId
-             ).get_result()
+                name=apikey_name,
+                iam_id=iam_id
+            ).get_result()
+
+            apikey_id = api_key['id']
 
             print(json.dumps(api_key, indent=2))
 
             # end-create_api_key
-
-            global apikeyId
-            apikeyId = api_key['id']
-
-        except ApiException as e:
-            pytest.fail(str(e))
-
-    @needscredentials
-    def test_get_api_key_example(self):
-        """
-        get_api_key request example
-        """
-        try:
-            # begin-get_api_key
-
-            api_key = iam_identity_service.get_api_key(
-                id= apikeyId
-            ).get_result()
-
-            print(json.dumps(api_key, indent=2))
-
-            # end-get_api_key
-
-            global apikeyEtag
-            apikeyEtag = api_key['entity_tag']
-
-        except ApiException as e:
-            pytest.fail(str(e))
-    
-    @needscredentials
-    def test_get_api_keys_details_example(self):
-        """
-        get_api_keys_details request example
-        """
-        try:
-            # begin-get_api_keys_details
-
-            api_key = iam_identity_service.get_api_keys_details(
-                iam_api_key= apikey
-            ).get_result()
-
-            print(json.dumps(api_key, indent=2))
-
-            # end-get_api_keys_details
 
         except ApiException as e:
             pytest.fail(str(e))
@@ -145,11 +114,14 @@ class TestIamIdentityV1Examples():
         list_api_keys request example
         """
         try:
+            global account_id, iam_id
+
             # begin-list_api_keys
 
             api_key_list = iam_identity_service.list_api_keys(
-                account_id= accountId,
-                iam_id= iamId
+                account_id=account_id,
+                iam_id=iam_id,
+                include_history=True
             ).get_result()
 
             print(json.dumps(api_key_list, indent=2))
@@ -160,16 +132,62 @@ class TestIamIdentityV1Examples():
             pytest.fail(str(e))
 
     @needscredentials
+    def test_get_api_keys_details_example(self):
+        """
+        get_api_keys_details request example
+        """
+        try:
+            global apikey
+
+            # begin-get_api_keys_details
+
+            api_key = iam_identity_service.get_api_keys_details(
+                iam_api_key=apikey
+            ).get_result()
+
+            print(json.dumps(api_key, indent=2))
+
+            # end-get_api_keys_details
+
+        except ApiException as e:
+            pytest.fail(str(e))
+
+    @needscredentials
+    def test_get_api_key_example(self):
+        """
+        get_api_key request example
+        """
+        try:
+            global apikey_id, apikey_etag
+            # begin-get_api_key
+
+            response = iam_identity_service.get_api_key(
+                id=apikey_id
+            )
+
+            apikey_etag = response.get_headers()['Etag']
+            api_key = response.get_result()
+
+            print(json.dumps(api_key, indent=2))
+
+            # end-get_api_key
+
+        except ApiException as e:
+            pytest.fail(str(e))
+
+    @needscredentials
     def test_update_api_key_example(self):
         """
         update_api_key request example
         """
         try:
+            global apikey_id, apikey_etag
             # begin-update_api_key
 
             api_key = iam_identity_service.update_api_key(
-                id= apikeyId,
-                if_match= apikeyEtag,
+                id=apikey_id,
+                if_match=apikey_etag,
+                description='This is an updated description'
             ).get_result()
 
             print(json.dumps(api_key, indent=2))
@@ -185,51 +203,49 @@ class TestIamIdentityV1Examples():
         lock_api_key request example
         """
         try:
+            global apikey_id
             # begin-lock_api_key
 
-            response = iam_identity_service.lock_api_key(
-                id= apikeyId
-            ).get_result()
+            response = iam_identity_service.lock_api_key(id=apikey_id)
 
-            print(json.dumps(response, indent=2))
+            print(response)
 
             # end-lock_api_key
 
         except ApiException as e:
             pytest.fail(str(e))
-    
+
     @needscredentials
     def test_unlock_api_key_example(self):
         """
         unlock_api_key request example
         """
         try:
+            global apikey_id
             # begin-unlock_api_key
 
-            response = iam_identity_service.unlock_api_key(
-                id= apikeyId
-            ).get_result()
+            response = iam_identity_service.unlock_api_key(id=apikey_id)
 
-            print(json.dumps(response, indent=2))
+            print(response)
 
             # end-unlock_api_key
 
         except ApiException as e:
             pytest.fail(str(e))
-    
+
     @needscredentials
     def test_delete_api_key_example(self):
         """
         delete_api_key request example
         """
         try:
+            global apikey_id
+
             # begin-delete_api_key
 
-            response = iam_identity_service.delete_api_key(
-                id= apikeyId
-            ).get_result()
+            response = iam_identity_service.delete_api_key(id=apikey_id)
 
-            print(json.dumps(response, indent=2))
+            print(response)
 
             # end-delete_api_key
 
@@ -242,20 +258,20 @@ class TestIamIdentityV1Examples():
         create_service_id request example
         """
         try:
+            global account_id, serviceid_name, svc_id
             # begin-create_service_id
 
             service_id = iam_identity_service.create_service_id(
-                account_id= accountId,
-                name= serviceIDName
+                account_id=account_id,
+                name=serviceid_name,
+                description='Example ServiceId'
             ).get_result()
+
+            svc_id = service_id['id']
 
             print(json.dumps(service_id, indent=2))
 
             # end-create_service_id
-
-            global serviceId
-            serviceId = service_id['id']
-
         except ApiException as e:
             pytest.fail(str(e))
 
@@ -265,34 +281,37 @@ class TestIamIdentityV1Examples():
         get_service_id request example
         """
         try:
+            global svc_id, svc_id_etag
             # begin-get_service_id
 
-            service_id = iam_identity_service.get_service_id(
-                id= serviceId
-            ).get_result()
+            response = iam_identity_service.get_service_id(
+                id=svc_id
+            )
+
+            svc_id_etag = response.get_headers()['Etag']
+            service_id = response.get_result()
 
             print(json.dumps(service_id, indent=2))
 
             # end-get_service_id
 
-            global serviceIDEtag
             serviceIDEtag = service_id['entity_tag']
 
         except ApiException as e:
             pytest.fail(str(e))
-    
+
     @needscredentials
     def test_list_service_ids_example(self):
         """
         list_service_ids request example
         """
         try:
+            global account_id, serviceid_name
             # begin-list_service_ids
 
             service_id_list = iam_identity_service.list_service_ids(
-                account_id= accountId,
-                name= serviceIDName,
-                pagesize= 100
+                account_id=account_id,
+                name=serviceid_name
             ).get_result()
 
             print(json.dumps(service_id_list, indent=2))
@@ -308,11 +327,13 @@ class TestIamIdentityV1Examples():
         update_service_id request example
         """
         try:
+            global svc_id, svc_id_etag
             # begin-update_service_id
 
             service_id = iam_identity_service.update_service_id(
-                id= serviceId,
-                if_match= serviceIDEtag,
+                id=svc_id,
+                if_match=svc_id_etag,
+                description='This is an updated description'
             ).get_result()
 
             print(json.dumps(service_id, indent=2))
@@ -328,10 +349,11 @@ class TestIamIdentityV1Examples():
         lock_service_id request example
         """
         try:
+            global svc_id
             # begin-lock_service_id
 
             service_id = iam_identity_service.lock_service_id(
-                id= serviceId
+                id=svc_id
             ).get_result()
 
             print(json.dumps(service_id, indent=2))
@@ -347,10 +369,12 @@ class TestIamIdentityV1Examples():
         unlock_service_id request example
         """
         try:
+            global svc_id
+
             # begin-unlock_service_id
 
             service_id = iam_identity_service.unlock_service_id(
-                id= serviceId
+                id=svc_id
             ).get_result()
 
             print(json.dumps(service_id, indent=2))
@@ -366,13 +390,13 @@ class TestIamIdentityV1Examples():
         delete_service_id request example
         """
         try:
+            global svc_id
+
             # begin-delete_service_id
 
-            response = iam_identity_service.delete_service_id(
-                id= serviceId
-            ).get_result()
+            response = iam_identity_service.delete_service_id(id=svc_id)
 
-            print(json.dumps(response, indent=2))
+            print(response)
 
             # end-delete_service_id
 
