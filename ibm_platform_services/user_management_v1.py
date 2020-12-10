d20e76f1eac4e604e100b145a294640a56b59983
Phil Adams <phil_adams@us.ibm.com>
2020-12-10 12:26:55 -0600

    fix(User Management): re-gen service with latest API

--- ibm_platform_services/user_management_v1.py
+++ ibm_platform_services/user_management_v1.py
@@ -14,7 +14,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-# IBM OpenAPI SDK Code Generator Version: 99-SNAPSHOT-60fd6b3d-20201128-071551
+# IBM OpenAPI SDK Code Generator Version: 99-SNAPSHOT-d753183b-20201209-163011
  
 """
 Manage the lifecycle of your users using User Management APIs.
