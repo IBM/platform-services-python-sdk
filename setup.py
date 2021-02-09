17dc9547b58ff5c8de5a029640ad1b20e323dd72
Phil Adams <phil_adams@us.ibm.com>
2021-02-09 16:48:09 -0600

    fix(build): transition default branch master -> main

--- setup.py
+++ setup.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
-# Copyright 2019 IBM All Rights Reserved.
+# Copyright 2019, 2021 IBM All Rights Reserved.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
