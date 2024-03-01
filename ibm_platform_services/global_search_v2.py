1a7d07d3a9fb2a8169cfcc37c99d45a1aceb525a
Phil Adams <phil_adams@us.ibm.com>
2024-03-01 10:21:44 -0600

    fix: fix lint errors
    Signed-off-by: Phil Adams <phil_adams@us.ibm.com>

--- ibm_platform_services/global_search_v2.py
+++ ibm_platform_services/global_search_v2.py
@@ -62,9 +62,7 @@ class GlobalSearchV2(BaseService):
                parameters and external configuration.
         """
         authenticator = get_authenticator_from_environment(service_name)
-        service = cls(
-            authenticator
-            )
+        service = cls(authenticator)
         service.configure_service(service_name)
         return service
 
@@ -261,6 +259,7 @@ class SearchEnums:
         TRUE = 'true'
         FALSE = 'false'
         ANY = 'any'
+
     class IsReclaimed(str, Enum):
         """
         Determines if reclaimed documents should be included in result set or not.
@@ -272,6 +271,7 @@ class SearchEnums:
         TRUE = 'true'
         FALSE = 'false'
         ANY = 'any'
+
     class IsPublic(str, Enum):
         """
         Determines if public resources should be included in result set or not. Possible
@@ -282,6 +282,7 @@ class SearchEnums:
         TRUE = 'true'
         FALSE = 'false'
         ANY = 'any'
+
     class CanTag(str, Enum):
         """
         Determines if the result set must return the resources that the user can tag or
--- ibm_platform_services/global_tagging_v1.py
+++ ibm_platform_services/global_tagging_v1.py
@@ -61,9 +61,7 @@ class GlobalTaggingV1(BaseService):
                parameters and external configuration.
         """
         authenticator = get_authenticator_from_environment(service_name)
-        service = cls(
-            authenticator
-            )
+        service = cls(authenticator)
         service.configure_service(service_name)
         return service
 
@@ -691,6 +689,7 @@ class ListTagsEnums:
         USER = 'user'
         SERVICE = 'service'
         ACCESS = 'access'
+
     class Providers(str, Enum):
         """
         Select a provider. Supported values are `ghost` and `ims`. To list both Global
@@ -701,6 +700,7 @@ class ListTagsEnums:
 
         GHOST = 'ghost'
         IMS = 'ims'
+
     class OrderByName(str, Enum):
         """
         Order the output by tag name.
@@ -735,6 +735,7 @@ class DeleteTagAllEnums:
 
         GHOST = 'ghost'
         IMS = 'ims'
+
     class TagType(str, Enum):
         """
         The type of the tag. Supported values are `user`, `service` and `access`.
@@ -760,6 +761,7 @@ class DeleteTagEnums:
 
         GHOST = 'ghost'
         IMS = 'ims'
+
     class TagType(str, Enum):
         """
         The type of the tag. Supported values are `user`, `service` and `access`.
@@ -1110,7 +1112,6 @@ class DeleteTagResultsItem:
 
         GHOST = 'ghost'
         IMS = 'ims'
-
 
 
 class DeleteTagsResult:
