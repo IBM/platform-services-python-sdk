62773e566b20859cc8a6d240f15a873504c9b951
fabrizio-leoni <fabrizio_leoni@it.ibm.com>
2024-05-28 14:14:28 +0200

    fix(Global Search): add support for is_project_resource query param
    Signed-off-by: Fabrizio Leoni <fabrizio_leoni@it.ibm.com>

--- test/unit/test_global_search_v2.py
+++ test/unit/test_global_search_v2.py
@@ -130,6 +130,7 @@ class TestSearch:
         is_public = 'false'
         impersonate_user = 'testString'
         can_tag = 'false'
+        is_project_resource = 'false'
 
         # Invoke method
         response = _service.search(
@@ -147,6 +148,7 @@ class TestSearch:
             is_public=is_public,
             impersonate_user=impersonate_user,
             can_tag=can_tag,
+            is_project_resource=is_project_resource,
             headers={},
         )
 
@@ -165,6 +167,7 @@ class TestSearch:
         assert 'is_public={}'.format(is_public) in query_string
         assert 'impersonate_user={}'.format(impersonate_user) in query_string
         assert 'can_tag={}'.format(can_tag) in query_string
+        assert 'is_project_resource={}'.format(is_project_resource) in query_string
         # Validate body params
         req_body = json.loads(str(responses.calls[0].request.body, 'utf-8'))
         assert req_body['query'] == 'testString'
